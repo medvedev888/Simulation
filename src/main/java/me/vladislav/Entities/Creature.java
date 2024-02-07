69eae5e63c15ce272a854c5fd0a29ad748ff734d
vladislavmedvedev <EnoT3581@mail.ru>
2024-02-07 20:38:30 +0300

    changed Entity.java and added Creature.java

---
+++ src/main/java/me/vladislav/Entities/Creature.java
@@ -0,0 +1,44 @@
+package me.vladislav.Entities;
+
+import me.vladislav.App.Map;
+import me.vladislav.Coordinates;
+
+public class Creature extends Entity {
+
+    private int hungerLevel;
+    private int strideLength;
+    private int healthPoints;
+
+    public Creature(Coordinates position, Map map, int hungerLevel, int strideLength, int healthPoints) {
+        super(position, map);
+        this.hungerLevel = hungerLevel;
+        this.strideLength = strideLength;
+        this.healthPoints = healthPoints;
+
+    }
+
+    public int getHungerLevel() {
+        return hungerLevel;
+    }
+
+    public void setHungerLevel(int hungerLevel) {
+        this.hungerLevel = hungerLevel;
+    }
+
+    public int getStrideLength() {
+        return strideLength;
+    }
+
+    public void setStrideLength(int strideLength) {
+        this.strideLength = strideLength;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+}
--- src/main/java/me/vladislav/Entities/Entity.java
+++ src/main/java/me/vladislav/Entities/Entity.java
@@ -3,9 +3,11 @@ package me.vladislav.Entities;
 import me.vladislav.App.Map;
 import me.vladislav.Coordinates;
 
+import java.util.Objects;
+
 public abstract class Entity {
     private Coordinates position;
-    private Map map;
+    private final Map map;
 
     public Entity(Coordinates position, Map map) {
         this.position = position;
@@ -19,4 +21,26 @@ public abstract class Entity {
     public void setPosition(Coordinates position) {
         this.position = position;
     }
+
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "position=" + position +
+                ", map=" + map +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entity entity = (Entity) o;
+        return Objects.equals(position, entity.position) && Objects.equals(map, entity.map);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, map);
+    }
+
 }
